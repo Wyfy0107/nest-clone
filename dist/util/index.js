828b26f36da9ec014cdae4c77b6bda16ec0b1867
Duy Nguyen <nguyenduy010798@gmail.com>
2021-07-25 15:29:06 +0300

    remove dist

--- dist/util/index.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.getInstanceMethodNames = void 0;
-const getInstanceMethodNames = (obj) => {
-    const proto = Object.getPrototypeOf(obj);
-    const names = Object.getOwnPropertyNames(proto);
-    return names.filter(name => typeof obj[name] === 'function' && name !== 'constructor');
-};
-exports.getInstanceMethodNames = getInstanceMethodNames;
